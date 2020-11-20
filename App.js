a7acef33362f0faa5576819a0db2bf589e68148d
ricard0vargas <53881227+ricard0vargas@users.noreply.github.com>
2020-11-19 19:59:21 -0600

    First Commit Example

---
+++ App.js
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {StyleSheet,ScrollView, Text, View} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+const App: () => React$Node = () => {
+  return (
+    <View> 
+      <Text>Hola Mundo</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
